aef6c202538babcc56b93b83a329a43425638e66
Santosh <santoshsolanki0716@gmail.com>
2022-10-28 19:01:03 +0530

    Initial commit2

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const About = () => {
+  return (
+    <div>
+      This is an About Component
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptatibus sit doloremque quis eveniet, consequatur eum ducimus temporibus ratione perspiciatis aperiam a. Magnam culpa libero vitae nihil veniam maxime impedit cum deserunt? Sunt, necessitatibus.</p>
+    </div>
+  )
+}
+
+export default About;
